0f0041283cff6c7f640cc81b18ee28e2af7cd372
Ybira <tamaskoszo@Tamas-MacBook-Air.local>
2018-01-28 01:39:54 +0100

    tkoszo-HOME

--- README.md
+++ README.md
@@ -82,3 +82,83 @@
   - __FB-devreg__ __genes__ __with__ __AS__ __ES/IR/ALT5SS/ALT3SS__ : alternative splicingban résztvevő FB-devreg gének AS eseményeinek száma | alternative splicingban résztvevő FB-devreg gének AS eseményeinek százalékos aránya
   - __FB-init__ __genes__ __with__ __AS__ __ES/IR/ALT5SS/ALT3SS__ : alternative splicingban résztvevő FB-init gének AS eseményeinek száma | alternative splicingban résztvevő FB-init gének AS eseményeinek százalékos aránya
 
+# Coprinopsis cinerea
+
+## Táblázat elkészítése
+
+### Script:
+- TABLE_ccinerea.R
+
+### Input:
+- __isoforms.fpkm_tracking__
+  - CuffDiff output FPKM érték táblázat az izoformákra
+- __genes.fpkm_tracking__
+  - CuffDiff output FPKM érték táblázat az génekre
+- __ccinerea_AmutBmut_AS_annotation.gtf__
+  - a végső annotációs fájl amivel a CuffDiff analízis is futott
+
+### Output:
+- __ccinerea_isoforms.tsv__
+  - __VM_FPKM_Rank__ -> __FB_T_FPKM_Rank__ __oszlopok__: megadja, hogy az adott izoforma expressziós értéke hanyadik helyre van sorolva az összes izoforma expressziós értéke közül az adott fejlődési fázisban/szövettípusban, megmutatja hogy az adott izoforma mennyire domináns az adott fejlődési fázisban/szövettípusban
+  - __Rank_Sum__ __oszlop__: az izoformák dominancia értékei/rank-jei összeadva az összes fejlődési fázisban/szövettípusban
+  - __Overall_FPKM__ __oszlok__: az izoformák teljes expressziós értéke az összes fejlődési fázisban/szövettípusban
+  - __isoforms__ : a gén izoformáinak mennyisége
+  - __isoform__ __switch__ : a rank-ok különböző fejlődési fázisok/szövettípusok közötti váltakozása miatt, megfigyelhető hogy egyes izoformák dominánsak-e egy fázisban majd a dominanciájukat elveszítik-e egy másikban
+  - __primary__ __transcript__ : azt a transzkriptet nevezzük primary transzkriptnek amelyik összeadott dominancia értéke (Rank_Sum) a legkisebb, vagyis a legdominánsabb az izoformák közül, amennyiben ezt az értéket több izoforma is felveszi, az izoforma összeexpressziójának értéke (Overall_FPKM) alapján döntünk, vagyis az lesz a primary transcript amelyiknek a legnagyobb az expressziós értéke az azonos rank-ú izoformák közül
+- __ccinerea_genes.tsv__
+  - __FB-devreg__
+    - legalább 4 FPKM értékkel rendelkezik H - FB között valamelyik fejlődési fázisban/szövettípusban
+    - legalább 4x Fold Change-el rendelkezik valamelyik fejlődési útvonalon
+      - DEVREG_T : H -> P1 -> P2 -> YFB_T -> FB_T
+      - DEVREG_K : H -> P1 -> P2 -> YFB_K -> FB_KL
+      - DEVREG_L : H -> P1 -> P2 -> YFB_L -> FB_KL
+      - DEVREG_YFB : YFB_T -> YFB_K -> YFB_L
+      - DEVREG_FB : FB_T -> FB_KL
+  - __FB-init__
+    - legalább 4 FPKM értékkel rendelkezik VM - P1 között valamelyik fejlődési fázisban/szövettípusban
+    - legalább 4x pozitív Fold Change-el rendelkezik VM - H+P1 átmenetben
+- __ccinerea_corrected_annotation.gtf__
+  - újrarendezzük az annotációs fájl sorait az izoforma dominancia alapján, hogy a primary transcript legyen elől, az ASpli ezután a primary transcripthez viszonyítva adja meg az alternative splicing eseményeket
+
+## Statisztikák kinyerése
+
+### Script:
+- STATS_aostoyae.R
+
+### Input:
+- __aostoyae_genes.tsv__
+- __aostoyae_isoforms.tsv__
+- __aostoyae_corrected_annotation.gtf__
+
+### Output:
+- __aostoyae_stats.tsv__
+  - a statisztikai adatokat tartalmazó fájl, részletezése az utolsó lépésnél
+- __aostoyae_AS_genes.gtf__
+  - alternative splicingban résztvevő géneket tartalmazó annotációs fájl az ASpli számára
+- __aostoyae_FB_DEVREG_genes.gtf__
+  - alternative splicingban résztvevő FB-devreg géneket tartalmazó annotációs fájl az ASpli számára
+- __aostoyae_FB_INIT_genes.gtf__
+  - alternative splicingban résztvevő FB-init géneket tartalmazó annotációs fájl az ASpli számára
+
+## Alternative Splicing statisztikák kinyerése
+
+### Script:
+- AS_aostoyae.R
+
+### Input:
+- __aostoyae_stats.tsv__
+- __aostoyae_AS_genes.gtf__
+- __aostoyae_FB_DEVREG_genes.gtf__
+- __aostoyae_FB_INIT_genes.gtf__
+
+### Output:
+- __aostoyae_stats.tsv__
+  - __genes__ : összes annotált gén
+  - __FB-devreg__ __genes__ : FB-devreg gének száma | FB-devreg gének százaléka az összes annotált génhez viszonyítva
+  - __FB-init__ __genes__ : FB-init gének száma | FB-init gének százaléka az összes annotált génhez viszonyítva
+  - __genes__ __with__ __AS__ : alternative splicingban résztvevő gének száma | alternative splicingban résztvevő gének százaléka az összes annotált génhez viszonyítva
+  - __FB-devreg__ __genes__ __with__ __AS__ : alternative splicingban résztvevő FB-devreg gének száma | alternative splicingban résztvevő FB-devreg gének százaléka az összes annotált génhez viszonyítva
+  - __FB-init__ __genes__ __with__ __AS__ : alternative splicingban résztvevő FB-init gének száma | alternative splicingban résztvevő FB-init gének százaléka az összes annotált génhez viszonyítva
+  - __genes__ __with__ __AS__ __ES/IR/ALT5SS/ALT3SS__ : alternative splicingban résztvevő gének AS eseményeinek száma | alternative splicingban résztvevő gének AS eseményeinek százalékos aránya
+  - __FB-devreg__ __genes__ __with__ __AS__ __ES/IR/ALT5SS/ALT3SS__ : alternative splicingban résztvevő FB-devreg gének AS eseményeinek száma | alternative splicingban résztvevő FB-devreg gének AS eseményeinek százalékos aránya
+  - __FB-init__ __genes__ __with__ __AS__ __ES/IR/ALT5SS/ALT3SS__ : alternative splicingban résztvevő FB-init gének AS eseményeinek száma | alternative splicingban résztvevő FB-init gének AS eseményeinek százalékos aránya
---
+++ TABLE_ccinerea.R
@@ -0,0 +1,232 @@
+library(tidyr)
+library(readr)
+library(dplyr)
+library(stringr)
+
+##### LOAD FILES #####
+
+setwd("~/Desktop/MTA_CMC_project/ccinerea/")
+
+isoforms <- read_tsv("isoforms.fpkm_tracking", col_names = T, cols(.default = col_guess(), tracking_id = col_character(), gene_id = col_character()))   
+isoforms <- isoforms %>%
+  select(transcript_id = tracking_id, gene_id, VM_FPKM, H_FPKM, P1_FPKM, P2_FPKM, YFB_K_FPKM, YFB_L_FPKM, YFB_T_FPKM, FB_KL_FPKM, FB_T_FPKM)            # kiválasztjuk a minket érdeklő oszlopokat
+isoforms <- isoforms[,c(2,1,3:11)]
+isoforms <- isoforms %>%
+  arrange(gene_id, transcript_id)
+
+annotation <- read_tsv("ccinerea_AmutBmut_AS_annotation.gtf",  col_names = c("chr", "maker","type", "start", "end", "att1", "strand", "att2", "attributes")) %>%
+  separate(attributes, c("transcriptID_label", "transcriptID", "geneID_label", "geneID"), sep = " ")
+annotation$transcriptID <- annotation$transcriptID %>% 
+  str_replace("\"", "") %>%
+  str_replace("\";", "")              # transcriptID-k átalakítása
+
+genes <- read_tsv("genes.fpkm_tracking", col_names = T, cols(.default = col_guess(), tracking_id = col_character(), gene_id = col_character()))     # betöltjük az isoform FPKM táblát
+genes <- genes %>% select(gene_id, VM_FPKM, H_FPKM, P1_FPKM, P2_FPKM, YFB_K_FPKM, YFB_L_FPKM, YFB_T_FPKM, FB_KL_FPKM, FB_T_FPKM)            # kiválasztjuk a minket érdeklő oszlopokat
+
+##### GENES - 4 FPKM H-FB #####
+
+for (x in 1:length(genes$gene_id)) {
+  if (max(genes[x,3:10]) >= 4) {
+    genes$">= 4 FPKM"[x] <- T
+  } else {
+    genes$">= 4 FPKM"[x] <- F
+  }
+  pb <- txtProgressBar(min = 1, max = length(genes$gene_id), style = 3)        
+  setTxtProgressBar(pb, x, title = NULL, label = NULL) 
+}
+
+##### GENES - DEVREG_T #####
+
+for (x in 1:length(genes$gene_id)) {
+  if (is.na((max(genes$H_FPKM[x], genes$P1_FPKM[x], genes$P2_FPKM[x], genes$YFB_T_FPKM[x], genes$FB_T_FPKM[x])/min(genes$H_FPKM[x], genes$P1_FPKM[x], genes$P2_FPKM[x], genes$YFB_T_FPKM[x], genes$FB_T_FPKM[x])))) {
+    genes$DEVREG_T[x] <- NA 
+  } else if ((max(genes$H_FPKM[x], genes$P1_FPKM[x], genes$P2_FPKM[x], genes$YFB_T_FPKM[x], genes$FB_T_FPKM[x])/min(genes$H_FPKM[x], genes$P1_FPKM[x], genes$P2_FPKM[x], genes$YFB_T_FPKM[x], genes$FB_T_FPKM[x])) >= 4) {
+    genes$DEVREG_T[x] <- T
+  } else {
+    genes$DEVREG_T[x] <- F
+  }
+  pb <- txtProgressBar(min = 1, max = length(genes$gene_id), style = 3)        
+  setTxtProgressBar(pb, x, title = NULL, label = NULL) 
+}
+
+##### GENES - DEVREG_K #####
+
+for (x in 1:length(genes$gene_id)) {
+  if (is.na((max(genes$H_FPKM[x], genes$P1_FPKM[x], genes$P2_FPKM[x], genes$YFB_K_FPKM[x], genes$FB_KL_FPKM[x])/min(genes$H_FPKM[x], genes$P1_FPKM[x], genes$P2_FPKM[x], genes$YFB_K_FPKM[x], genes$FB_KL_FPKM[x])))) {
+    genes$DEVREG_K[x] <- NA 
+  } else if ((max(genes$H_FPKM[x], genes$P1_FPKM[x], genes$P2_FPKM[x], genes$YFB_K_FPKM[x], genes$FB_KL_FPKM[x])/min(genes$H_FPKM[x], genes$P1_FPKM[x], genes$P2_FPKM[x], genes$YFB_K_FPKM[x], genes$FB_KL_FPKM[x])) >= 4) {
+    genes$DEVREG_K[x] <- T
+  } else {
+    genes$DEVREG_K[x] <- F
+  }
+  pb <- txtProgressBar(min = 1, max = length(genes$gene_id), style = 3)        
+  setTxtProgressBar(pb, x, title = NULL, label = NULL) 
+}
+
+##### GENES - DEVREG_L #####
+
+for (x in 1:length(genes$gene_id)) {
+  if (is.na((max(genes$H_FPKM[x], genes$P1_FPKM[x], genes$P2_FPKM[x], genes$YFB_L_FPKM[x], genes$FB_KL_FPKM[x])/min(genes$H_FPKM[x], genes$P1_FPKM[x], genes$P2_FPKM[x], genes$YFB_L_FPKM[x], genes$FB_KL_FPKM[x])))) {
+    genes$DEVREG_L[x] <- NA 
+  } else if ((max(genes$H_FPKM[x], genes$P1_FPKM[x], genes$P2_FPKM[x], genes$YFB_L_FPKM[x], genes$FB_KL_FPKM[x])/min(genes$H_FPKM[x], genes$P1_FPKM[x], genes$P2_FPKM[x], genes$YFB_L_FPKM[x], genes$FB_KL_FPKM[x])) >= 4) {
+    genes$DEVREG_L[x] <- T
+  } else {
+    genes$DEVREG_L[x] <- F
+  }
+  pb <- txtProgressBar(min = 1, max = length(genes$gene_id), style = 3)        
+  setTxtProgressBar(pb, x, title = NULL, label = NULL) 
+}
+
+##### GENES - DEVREG_YFB #####
+
+for (x in 1:length(genes$gene_id)) {
+  if (is.na((max(genes$YFB_K_FPKM[x], genes$YFB_L_FPKM[x], genes$YFB_T_FPKM[x])/min(genes$YFB_K_FPKM[x], genes$YFB_L_FPKM[x], genes$YFB_T_FPKM[x])))) {
+    genes$DEVREG_YFB[x] <- NA 
+  } else if ((max(genes$YFB_K_FPKM[x], genes$YFB_L_FPKM[x], genes$YFB_T_FPKM[x])/min(genes$YFB_K_FPKM[x], genes$YFB_L_FPKM[x], genes$YFB_T_FPKM[x])) >= 4) {
+    genes$DEVREG_YFB[x] <- T
+  } else {
+    genes$DEVREG_YFB[x] <- F
+  }
+  pb <- txtProgressBar(min = 1, max = length(genes$gene_id), style = 3)        
+  setTxtProgressBar(pb, x, title = NULL, label = NULL) 
+}
+
+##### GENES - DEVREG_FB #####
+
+for (x in 1:length(genes$gene_id)) {
+  if (is.na((max(genes$FB_T_FPKM[x], genes$FB_KL_FPKM[x])/min(genes$FB_T_FPKM[x], genes$FB_KL_FPKM[x])))) {
+    genes$DEVREG_FB[x] <- NA 
+  } else if ((max(genes$FB_T_FPKM[x], genes$FB_KL_FPKM[x])/min(genes$FB_T_FPKM[x], genes$FB_KL_FPKM[x])) >= 4) {
+    genes$DEVREG_FB[x] <- T
+  } else {
+    genes$DEVREG_FB[x] <- F
+  }
+  pb <- txtProgressBar(min = 1, max = length(genes$gene_id), style = 3)        
+  setTxtProgressBar(pb, x, title = NULL, label = NULL) 
+}
+
+##### GENES - FB-devreg #####
+
+for (x in 1:length(genes$gene_id)) {
+   if (genes$`>= 4 FPKM`[x] == T & (genes$DEVREG_T[x] == T | 
+                                    genes$DEVREG_K[x] == T |
+                                    genes$DEVREG_L[x] == T | 
+                                    genes$DEVREG_YFB[x] == T |
+                                    genes$DEVREG_FB[x] == T) == T) {
+    genes$"FB-devreg"[x] <- T
+  } else {
+    genes$"FB-devreg"[x] <- F
+  }
+  pb <- txtProgressBar(min = 1, max = length(genes$gene_id), style = 3)        
+  setTxtProgressBar(pb, x, title = NULL, label = NULL) 
+}
+
+genes <- genes[,c(1:10,17)]
+
+##### GENES - 4 FPKM VM-P1 #####
+
+for (x in 1:length(genes$gene_id)) {
+  if (max(genes[x,2:4]) >= 4) {
+    genes$">= 4 FPKM"[x] <- T
+  } else {
+    genes$">= 4 FPKM"[x] <- F
+  }
+  pb <- txtProgressBar(min = 1, max = length(genes$gene_id), style = 3)        
+  setTxtProgressBar(pb, x, title = NULL, label = NULL) 
+}
+
+##### GENES - FB-init #####
+
+for (x in 1:length(genes$gene_id)) {
+  if (genes$`>= 4 FPKM`[x] == T & ((max(genes$H_FPKM[x], genes$P1_FPKM[x]) / genes$VM_FPKM[x]) >= 4) == T) {
+    genes$"FB-init"[x] <- T
+  } else {
+    genes$"FB-init"[x] <- F
+  }
+  pb <- txtProgressBar(min = 1, max = length(genes$gene_id), style = 3)        
+  setTxtProgressBar(pb, x, title = NULL, label = NULL) 
+}
+
+genes <- genes[,c(1:11,13)]
+
+##### ISOFORM - FPKM RANKING #####
+
+isoforms.rank <- as_tibble()
+
+for (x in 1:length(unique((isoforms$gene_id)))) {
+  temp <- isoforms[isoforms$gene_id %in% unique(isoforms$gene_id)[x],]
+  temp$VM_FPKM_Rank <- match(temp$VM_FPKM,sort(temp$VM_FPKM, decreasing = T))
+  temp$H_FPKM_Rank <- match(temp$H_FPKM,sort(temp$H_FPKM, decreasing = T))
+  temp$P1_FPKM_Rank <- match(temp$P1_FPKM,sort(temp$P1_FPKM, decreasing = T))
+  temp$P2_FPKM_Rank <- match(temp$P2_FPKM,sort(temp$P2_FPKM, decreasing = T))
+  temp$YFB_K_FPKM_Rank <- match(temp$YFB_K_FPKM,sort(temp$YFB_K_FPKM, decreasing = T))
+  temp$YFB_L_FPKM_Rank <- match(temp$YFB_L_FPKM,sort(temp$YFB_L_FPKM, decreasing = T))
+  temp$YFB_T_FPKM_Rank <- match(temp$YFB_T_FPKM,sort(temp$YFB_T_FPKM, decreasing = T))
+  temp$FB_KL_FPKM_Rank <- match(temp$FB_KL_FPKM,sort(temp$FB_KL_FPKM, decreasing = T))
+  temp$FB_T_FPKM_Rank <- match(temp$FB_T_FPKM,sort(temp$FB_T_FPKM, decreasing = T))
+  for(y in 1:length(temp$gene_id)) {
+    temp$Rank_Sum[y] <- sum(temp$VM_FPKM_Rank[y],
+                         temp$H_FPKM_Rank[y],
+                         temp$P1_FPKM_Rank[y],
+                         temp$P2_FPKM_Rank[y],
+                         temp$YFB_K_FPKM_Rank[y],
+                         temp$YFB_L_FPKM_Rank[y],
+                         temp$YFB_T_FPKM_Rank[y],
+                         temp$FB_KL_FPKM_Rank[y],
+                         temp$FB_T_FPKM_Rank[y])
+    temp$Overall_FPKM[y] <- sum(temp$VM_FPKM[y],
+                            temp$H_FPKM[y],
+                            temp$P1_FPKM[y],
+                            temp$P2_FPKM[y],
+                            temp$YFB_K_FPKM[y],
+                            temp$YFB_L_FPKM[y],
+                            temp$YFB_T_FPKM[y],
+                            temp$FB_KL_FPKM[y],
+                            temp$FB_T_FPKM[y])
+  }
+  temp <- arrange(temp, Rank_Sum, desc(Overall_FPKM))
+  isoforms.rank <- rbind(isoforms.rank, temp)
+  pb <- txtProgressBar(min = 1, max = length(unique((isoforms$gene_id))), style = 3)        
+  setTxtProgressBar(pb, x, title = NULL, label = NULL) 
+}
+
+##### GENES - isoforms #####
+
+genes.AS <- as_tibble()
+
+for (x in 1:length(genes$gene_id)) {
+  temp <- genes[genes$gene_id %in% genes$gene_id[x],]
+  temp$isoforms <- nrow(isoforms.rank[isoforms.rank$gene_id %in% genes$gene_id[x],])
+  genes.AS <- rbind(genes.AS, temp)
+  pb <- txtProgressBar(min = 1, max = length(genes$gene_id), style = 3)        
+  setTxtProgressBar(pb, x, title = NULL, label = NULL) 
+}
+
+##### ANNOTATION CORRECTION #####
+
+annotation.corrected <- as_tibble()
+
+for (x in 1:length(isoforms.rank$transcript_id)) {
+  temp <- annotation[annotation$transcriptID %in% isoforms.rank$transcript_id[x],]
+  annotation.corrected <- rbind(annotation.corrected, temp)
+  pb <- txtProgressBar(min = 1, max = length(isoforms.rank$transcript_id), style = 3)        
+  setTxtProgressBar(pb, x, title = NULL, label = NULL) 
+}
+
+sum(unique(annotation.corrected$transcriptID) != isoforms.rank$transcript_id)
+
+##### WRITE FILES #####
+
+write_tsv(isoforms.rank, "ccinerea_isoforms.tsv")
+
+write_tsv(genes.AS, "ccinerea_genes.tsv")
+
+annotation.corrected$transcriptID <- annotation.corrected$transcriptID %>% 
+  str_replace("^", "\"") %>%
+  str_replace("$", "\";")              # transcriptID-k átalakítása
+
+merged <- unite(annotation.corrected, attributes, 9:12, sep = " ")
+
+write.table(merged, file = "ccinerea_corrected_annotation.gtf", row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
+
+
